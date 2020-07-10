5077c04615f1626480c2097f747d3cfa99e051b9
Neil Swainston <neil.swainston@liverpool.ac.uk>
2020-07-10 13:38:00 +0100

    Updated script to allow specification of out file.

--- fragment.sh
+++ fragment.sh
@@ -3,6 +3,6 @@ java \
 	-cp target/liv-metfrag-0.1.0-SNAPSHOT/WEB-INF/classes/:target/liv-metfrag-0.1.0-SNAPSHOT/WEB-INF/lib/* \
 	uk.ac.liverpool.metfrag.MetFragFragmenter \
 	$1 \
-	out/spectra.csv \
 	$2 \
-	$3
+	$3 \
+	$4
