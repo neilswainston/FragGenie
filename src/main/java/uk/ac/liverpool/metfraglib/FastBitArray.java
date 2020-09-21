2787ebf7c2571ca2822b6443925de699db3d6d96
Neil Swainston <neil.swainston@liverpool.ac.uk>
2020-09-21 20:57:21 +0100

    Simplified FastBitArray through extension.

--- src/main/java/uk/ac/liverpool/metfraglib/FastBitArray.java
+++ src/main/java/uk/ac/liverpool/metfraglib/FastBitArray.java
@@ -6,13 +6,13 @@ import javolution.util.FastBitSet;
  * 
  * @author neilswainston
  */
-class FastBitArray {
-
+class FastBitArray extends FastBitSet {
+	
 	/**
-	 * array to store bits
+	 * 
 	 */
-	private final FastBitSet bitArray;
-
+	private static final long serialVersionUID = 1L;
+	
 	/**
 	 * 
 	 */
@@ -25,48 +25,15 @@ class FastBitArray {
 	 * @param set
 	 */
 	FastBitArray(final int size, final boolean set) {
-		this.bitArray = new FastBitSet();
 		this.size = size;
 		
 		if(set) {
-			this.bitArray.flip(0, size);
+			this.flip(0, size);
 		}
 	}
 	
 	/**
-	 * Set bit at position idx to true.
-	 * 
-	 * Warning: function does not check for IndexOutOfBounds
-	 * 
-	 * @param idx
-	 */
-	void set(final int idx) {
-		this.bitArray.set(idx);
-	}
-
-	/**
-	 * set bit at position n to given value val
-	 * Warning: function does not check for IndexOutOfBounds
-	 * 
-	 * @param index
-	 */
-	void set(int n, boolean val) {
-		this.bitArray.set(n, val);
-	}
-	
-	/**
-	 * returns bit at index n
-	 * returns false if n < 0 and n > size of BitArray
-	 * 
-	 * @param n
-	 * @return
-	 */
-	boolean get(int n) {
-		return this.bitArray.get(n);
-	}
-	
-	/**
-	 * returns BitArray number of bits
+	 * Returns number of bits.
 	 * 
 	 * @return int
 	 */
@@ -75,42 +42,34 @@ class FastBitArray {
 	}
 	
 	/**
-	 * get integer array of indices that are set to true
+	 * Get integer array of indices that are set to true.
 	 * 
 	 * @return int[]
 	 */
 	int[] getSetIndices() {
-		int[] setIndeces = new int[this.bitArray.cardinality()];
+		final int[] setIndices = new int[this.cardinality()];
+		
 		int index = 0;
-		for(int i = 0; i < this.getSize(); i++) {
-			if(this.bitArray.get(i)) {
-				setIndeces[index] = i;
-				index++;
+		
+		for(int i = 0; i < this.size; i++) {
+			if(this.get(i)) {
+				setIndices[index++] = i;
 			}
 		}
-		return setIndeces;
-	}
-	
-	/**
-	 * returns string with with true positions '1' and false positions '0' 
-	 */
-	@Override
-	public String toString() {
-		char[] set = new char[this.getSize()];
-		for(int i = 0; i < this.getSize(); i++) {
-			set[i] = this.bitArray.get(i) ? '1' : '0';
-		}
-		return String.valueOf(set);
+		return setIndices;
 	}
 	
 	/**
 	 * returns last position of BitArray set to true
 	 * returns -1 if there is no bit set to true
-	 * 
 	 */
 	int getLastSetBit() {
-		for(int i = this.getSize() - 1; i >= 0; i--)
-			if(this.bitArray.get(i)) return i;
+		for(int i = this.getSize() - 1; i >= 0; i--) {
+			if(this.get(i)) {
+				return i;
+			}
+		}
+		
 		return -1;
 	}
 	
@@ -119,17 +78,33 @@ class FastBitArray {
 	 * 
 	 * @param bitIndexes
 	 */
-	void setBits(int[] bitIndexes) {
+	void setBits(final int[] bitIndexes) {
 		for(int i = 0; i < bitIndexes.length; i++) {
-			this.bitArray.set(bitIndexes[i]);
+			this.set(bitIndexes[i]);
 		}
 	}
 	
 	@Override
+	public String toString() {
+		final char[] set = new char[this.size];
+		
+		for(int i = 0; i < this.size; i++) {
+			set[i] = this.get(i) ? '1' : '0';
+		}
+		
+		return String.valueOf(set);
+	}
+	
+	@Override
 	public FastBitArray clone() {
-		FastBitArray clone = new FastBitArray(this.getSize(), false);
-		for(int i = 0; i < this.getSize(); i++)
-			if(this.bitArray.get(i)) clone.set(i);
+		final FastBitArray clone = new FastBitArray(this.size, false);
+		
+		for(int i = 0; i < this.getSize(); i++) {
+			if(this.get(i)) {
+				clone.set(i);
+			}
+		}
+		
 		return clone;
 	}
 }
