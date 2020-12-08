4bc9f620069da714457f5e87fcfe351e4ad4e546
Neil Swainston <neil.swainston@liverpool.ac.uk>
2020-12-08 12:22:58 +0000

    Updated shell script to read war file.

--- fragment.sh
+++ fragment.sh
@@ -2,6 +2,6 @@ mvn install
 
 java \
 	-Xmx32000m \
-	-cp target/liv-metfrag-0.1.0-SNAPSHOT.jar:target/lib/* \
+	-cp target/liv-metfrag-0.1.0-SNAPSHOT.war:target/lib/* \
 	uk.ac.liverpool.metfrag.MetFragFragmenter \
 	"$@"
