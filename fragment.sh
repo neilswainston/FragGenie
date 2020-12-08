d13ab9aadc65c4c7d05f0cd72a98d300ac9d0094
Neil Swainston <neil.swainston@liverpool.ac.uk>
2020-12-08 21:34:15 +0000

    Repackaged such that fragment.sh script generates jar (not war) file.

--- fragment.sh
+++ fragment.sh
@@ -1,7 +1,7 @@
-mvn install
+mvn -f pom_jar.xml install
 
 java \
 	-Xmx32000m \
-	-cp target/liv-metfrag-0.1.0-SNAPSHOT.war:target/lib/* \
+	-cp target/liv-metfrag-0.1.0-SNAPSHOT.jar:target/lib/* \
 	uk.ac.liverpool.metfrag.MetFragFragmenter \
 	"$@"
