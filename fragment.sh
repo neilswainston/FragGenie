ef3798e810f2723347b9df234549cd900429000b
Neil Swainston <neil.swainston@liverpool.ac.uk>
2021-04-28 14:34:44 +0100

    Update incorrectly defined path.

--- fragment.sh
+++ fragment.sh
@@ -4,6 +4,6 @@ mvn -f $BASEDIR/pom_jar.xml install
 
 java \
 	-Xmx32000m \
-	-cp $BASEDIR/target/liv-metfrag-0.1.0-SNAPSHOT.jar:target/lib/* \
+	-cp $BASEDIR/target/liv-metfrag-0.1.0-SNAPSHOT.jar:$BASEDIR/target/lib/* \
 	uk.ac.liverpool.metfrag.MetFragFragmenter \
 	"$@"
